fb241b637204359181e0d2fd280dad78f7729024
gaudelbijay <gaudelbijay10@gmail.com>
2020-03-24 01:41:25 +0800

    question one

---
+++ 01twoSum/twoSum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>m;
+        vector<int>result;
+        for (auto i =0; i<nums.size();i++){
+            if(m.find(nums[i])==m.end())
+                m[target-nums[i]]=i;
+            else{
+                result.push_back(m[nums[i]]+1);
+                result.push_back(i+1);
+            }
+        }
+        return result;
+    }
+};
